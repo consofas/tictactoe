61199e12f0f6d9ff1a1212122cdeb9f93a40dbd5
Rohan Catsburg <consofas@gmail.com>
2021-11-08 11:02:47 +1300

    Set state to ‘X’ when square is clicked
    Signed-off-by: Rohan Catsburg <consofas@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            values: null,
+        };
+    }
+
 	render() {
-		return <button className="square" onClick={() => {console.log('click'); }}>{this.props.value}</button>;
+		return <button className="square" onClick={() => this.setState({value: 'X'})}>{this.state.value}</button>;
 	}
 }
 
